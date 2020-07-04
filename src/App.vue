1332cb114fc24fb95df9ab386e7d3e706c483a29
leoboyerbx <contact@leoboyer.fr>
2020-07-04 13:21:56 +0200

    first step

---
+++ src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="app">
+    <div class="main-title">
+      <h1>Nethergonnaend</h1>
+      <p>Nether gonna give you up</p>
+    </div>
+    <div class="row">
+      <div class="col">
+        <Card class="status">
+          <h2>Statut</h2>
+          <p>Beueu</p>
+        </Card>
+      </div>
+      <div class="col">
+        <Card class="players">
+          <h2>Joueurs en ligne</h2>
+          <p>Hello</p>
+        </Card>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Card from './components/Card.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Card
+  }
+}
+</script>
+
+<style lang="scss">
+@import 'bootstrap/scss/bootstrap-grid';
+@import "scss/breakpoints.scss";
+@import url("https://use.typekit.net/qxr4owc.css");
+@import 'scss/globals.scss';
+
+body {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+}
+#app {
+  position: fixed;
+  width: 100%;
+  max-width: 860px;
+  color: $theme;
+}
+.main-title {
+  font-family: 'Europa Light', sans-serif;
+  text-align: center;
+  margin-bottom: 30px;
+}
+.col {
+  padding: 0 10px;
+}
+</style>
---
+++ src/components/Card.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="card">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Card'
+}
+</script>
+
+<style lang="scss">
+@import "../scss/globals.scss";
+
+.card{
+    // width: 100%;
+    font-family: 'Montserrat', sans-serif;
+    padding: 20px 40px;
+    box-shadow: 0px 0px 15px #00000049;
+    border-radius: 10px;
+    h2 {
+        text-align: center;
+        font-size: 1.4em;
+        margin: 0;
+        font-weight: bold;
+    }
+}
+</style>
