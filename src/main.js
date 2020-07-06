6c12aa84c525f81071a8919be78f0169c6505b97
leoboyerbx <leoboyer.bx@gmail.com>
2020-07-06 18:03:00 +0200

    Added socket io dependencies, code broken for now

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Socketio from 'socket.io-client'
+import VueSocketIO from 'vue-socket.io'
+
+export const SocketInstance = new Socketio('http://nethergonnaend.cf:3654')
+
+Vue.use(VueSocketIO, SocketInstance)
 
 Vue.config.productionTip = false
 
